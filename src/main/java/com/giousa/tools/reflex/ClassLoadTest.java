0a4e54f39ea236772d5a3957b63e180594632448
Giousa <65489469@qq.com>
2022-10-25 00:17:39 +0800

    加载器介绍

--- src/main/java/com/giousa/tools/reflex/ClassLoadTest.java
+++ src/main/java/com/giousa/tools/reflex/ClassLoadTest.java
@@ -3,6 +3,13 @@ package com.giousa.tools.reflex;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
+/**
+ * 加载器介绍
+ * 启动类加载器Bootstrap ClassLoader:加载JRE_HOME/lib下的核心包，该类加载器是用c++写的。
+ * 扩展类加载器Extension ClassLoader:加载JRE_HOME/lib/ext目录下的扩展包,也可以通过启动参数-Djava.ext.dirs指定，该类用java编写。对应ExtClassLoader类
+ * 应用类加载器Application ClassLoader:应用类加载器，加载classpath下的字节码文件，用java编写，对应AppClassLoader这个类，可以通过ClassLoader类的静态方法getSystemClassLoader()获得，所以又叫系统类加载器
+ * 自定义类加载器：自定义的类加载器，通过直接或者间接继承抽象的ClassLoader类。
+ */
 public class ClassLoadTest {
 
     private static final String className = "com.giousa.tools.reflex.Student";
